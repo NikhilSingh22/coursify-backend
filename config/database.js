89e2a8a043b7faa5ef17c1d9196b7505abfedb29
Nikhil Singh <ns2125560@gmail.com>
2023-03-05 12:04:44 +0530

    change in databasejs

--- config/database.js
+++ config/database.js
@@ -1,6 +1,15 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const { connection } = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`mongoDB connected with ${connection.host}`);
+  try {
+    const { connection } = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log(`MongoDB connected with ${connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error}`);
+  }
 };
