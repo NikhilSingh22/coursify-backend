0c1c136c6aaaf4cd97185362c687cdfd1c818eba
Nikhil Singh <ns2125560@gmail.com>
2023-03-05 12:17:54 +0530

    database change 2

--- config/database.js
+++ config/database.js
@@ -2,12 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const { connection } = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+    const { connection } = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected with ${connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error}`);
